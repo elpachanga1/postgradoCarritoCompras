1011616bb3262f86bf42d7ae3b9ead68d5dabf36
elpachanga1 <fabianguerrero9328@gmail.com>
2024-03-08 18:21:41 -0500

    delete base/dummy repository

--- Carrito-de-compras-Basic_React/src/entities/Interfaces.ts
+++
@@ -1,9 +0,0 @@
-export interface Product {
-    id: number;
-    sku: string;
-    name: string;
-    description?: string;
-    availableUnits: number;
-    unitPrice: number;
-    image?: string;
-}
